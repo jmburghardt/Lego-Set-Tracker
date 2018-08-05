b88c0df0c68fa77f4b212e23619ff421b778c853
Joe Burghardt <jmburghardt@gmail.com>
2018-08-05 15:25:45 -0500

    Initial commit

---
+++ src/main/java/com/SetTracker/legosettracker/LegoSetTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.SetTracker.legosettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoSetTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegoSetTrackerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/SetTracker/legosettracker/LegoSetTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.SetTracker.legosettracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LegoSetTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
